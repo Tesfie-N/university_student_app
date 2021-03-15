1e256b24eb3668183c39e786c17812d264e9926f
Tesfabrhan Naizghi <tesfienaiz@gmail.com>
2021-03-15 21:03:25 +0300

    added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -7,123 +6,88 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _text = TextEditingController();
+  bool _isEmptyEmail = false;
 
-  bool _isUniversityId = false;
-  TextEditingController _editingControllerUnId;
-  String universityID = "Enter University ID";
+  final _password = TextEditingController();
+  bool _isEmptyPassword = false;
 
-  String privateCode = "Enter private code";
-  bool _isPrivateCode = false;
-  TextEditingController _editingControllerPrCode;
-
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Login Page'),
         backgroundColor: Colors.cyan[700],
         centerTitle: true,
       ),
-
       body: Padding(
         padding: EdgeInsets.fromLTRB(50, 150, 50, 0),
-        child: Center(
-          child: Column(
-            children: [
-              _editEmail(),
-              SizedBox(height: 10),
-              _editPassword(),
-              SizedBox(height: 10),
-              RaisedButton(
-                onPressed: () {Navigator.pushNamed(context, '/id');
-                },
-                child: Text('Login',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
-                  ),),
-                color: Colors.cyan[900],
-              ),
-            ],
-          ),
-        ),
+        child: Center(child: Builder(
+          builder: (context) {
+              return Container(
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _text,
+                      decoration: InputDecoration(
+                        labelText: 'Enter email',
+                        errorText:
+                            _isEmptyEmail ? 'email cant\'t be empity' : null,
+                        hoverColor: Colors.red[900],
+                      ), 
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: _password,
+                      decoration: InputDecoration(
+                        labelText: 'Enter password',
+                        errorText: _isEmptyPassword
+                            ? 'pasword cant\'t be empity'
+                            : null,
+                        hoverColor: Colors.red[900],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    RaisedButton(
+                      onPressed: () {
+                        if (_text.text.isEmpty || _password.text.isEmpty) {
+                          setState(() {
+                            if (_text.text.isEmpty) _isEmptyEmail = true;
+
+                            if (_password.text.isEmpty) _isEmptyPassword = true;
+                          });
+                        } else
+                          Navigator.pushNamed(context, '/id');
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          letterSpacing: 2.0,
+                        ),
+                      ),
+                      color: Colors.cyan[900],
+                    ),
+                  ],
+                ),
+              );
+          },
+        )),
       ),
     );
   }
 
   @override
-  void initState() {
-    super.initState();
-    _editingControllerUnId = TextEditingController(text: universityID);
-    _editingControllerPrCode= TextEditingController(text: privateCode);
-  }
-
-  @override
   void dispose() {
-    _editingControllerUnId.dispose();
-    _editingControllerPrCode.dispose();
+    _password.dispose();
+    _text.dispose();
     super.dispose();
-  }
-
-  Widget _editEmail() {
-    if (_isUniversityId)
-      return Center(
-        child: TextField(
-          onSubmitted: (newValue){
-            setState(() {
-              universityID = newValue;
-              _isUniversityId =false;
-            });
-          },
-          autofocus: true,
-          controller: _editingControllerUnId,
-        ),
-      );
-    return InkWell(
-        onTap: () {
-          setState(() {
-            _isUniversityId = true;
-          });
-        },
-        child: Text(
-          universityID,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18.0,
-          ),
-        )
-    );
-  }
-
-  Widget _editPassword() {
-    if (_isPrivateCode)
-      return Center(
-        child: TextField(
-          onSubmitted: (newValue){
-            setState(() {
-              privateCode = newValue;
-              _isPrivateCode =false;
-            });
-          },
-          autofocus: true,
-          controller: _editingControllerPrCode,
-        ),
-      );
-    return InkWell(
-        onTap: () {
-          setState(() {
-            _isPrivateCode = true;
-          });
-        },
-        child: Text(
-          privateCode,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18.0,
-          ),
-        )
-    );
   }
 }
